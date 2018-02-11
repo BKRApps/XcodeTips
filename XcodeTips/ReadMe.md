2a6fef63c42b0250a629a1813f181440de08f685
kumar reddy <kumar.reddy@swiggy.in>
2018-02-11 19:55:13 +0530

    Added Xcode Tips for creating Color Set from xcassets

---
+++ XcodeTips/ReadMe.md
@@ -0,0 +1,2 @@
+1. use color literal to visually see the color.
+2. use New Color Set from Assets.xcassets to create a global color set for the application. It might be useful for theme purpose.
---
+++ XcodeTips/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  XcodeTips
+//
+//  Created by kumar reddy on 10/02/18.
+//  Copyright © 2018 BKRApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.8379951384, green: 0.2183702257, blue: 0.3824598524, alpha: 1)
+    }
+}
+
